e43e3e2c5a3bb5c724f457edc4e99f62c1eb6243
Vit <besteffects@gmail.com>
2020-09-07 11:36:14 -0400

    Raise

---
+++ basics/exceptions/Raise.py
@@ -0,0 +1,33 @@
+import sys
+
+DIGIT_MAP = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+
+def convert(s):
+    try:
+        number = ''
+        for token in s:
+            number += DIGIT_MAP[token]
+        return int(number)
+    except (KeyError, TypeError) as e:
+        print(f"Conversion error: {e!r}",
+              file=sys.stderr)
+        raise
+
+
+if __name__ == '__main__':
+    print('Testing exceptions: ')
+    print(convert('one three three seven'.split()))
+    print(convert('one three three seven ten'.split()))
+    print(convert(512))
