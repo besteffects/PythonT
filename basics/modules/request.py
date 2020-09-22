c9e4c1495554f633c828d338c0c0d49f655396d4
Vit <besteffects@gmail.com>
2020-09-21 21:25:53 -0400

    requests

--- basics/modules/request.py
+++ basics/modules/request.py
@@ -29,6 +29,18 @@ def get_spam_data():
               item['price'], sep='')
 
 
+def get_temperature():
+    url = "http://api.openweathermap.org/data/2.5/weather?q=Orlando,fl&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0"
+    request = requests.get(url)
+
+    weather_json = request.json()
+    print(weather_json)
+    main_weather = weather_json['main']
+    temp = main_weather['temp']
+    print("The Circus's current temperature is ", temp)
+
+
 if __name__ == '__main__':
     get_data()
     get_spam_data()
+    get_temperature()
