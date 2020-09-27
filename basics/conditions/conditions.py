5ac3e26f1dcc1fd7fb03c16078c66828ac1c79aa
Vit <besteffects@gmail.com>
2020-09-27 11:22:28 -0400

    conditions

---
+++ basics/conditions/conditions.py
@@ -0,0 +1,7 @@
+def print_color():
+    alien_color = ['green', 'yellow', 'red']
+    if alien_color == "green":
+        print(f"Alien color is {alien_color}. You have earned 5 points")
+    else:
+        print(f"Alien color isn't green. You have earned `0 points")
+        
