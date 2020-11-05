9c4ca1a15c79a04c0c709bb9cd18378e202d7dfb
Vit <besteffects@gmail.com>
2020-11-05 18:06:55 -0500

    Context manager

--- intermediate/context_managers/logging_context_manager.py
+++ intermediate/context_managers/logging_context_manager.py
@@ -4,12 +4,16 @@ class LoggingContextManager:
         return 'You are in a with-block!'
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('LoggingContextManager.__exit__({}, {}, {}'.format(
-            exc_type, exc_val, exc_tb))
-        return
+        if exc_type is None:
+            print('LoggingContextManager.__exit__:'
+                  'normal exit detected.')
+        else:
+            print('LoggingContextManager.__exit__:'
+                  'Exception detected!'
+                  'type={}, value={}, traceback={}'.format(
+                exc_type, exc_val, exc_tb))
 
 
 with LoggingContextManager() as x:  # Calling context manager
     print(x)
     raise ValueError('Something has gone wrong!')
-
